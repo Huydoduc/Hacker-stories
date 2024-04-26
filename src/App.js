e68f580f6d2471c4e76fc2d5225a72e34652f9f4
unknown <doduchuy19981998@gmail.com>
2024-04-26 16:50:58 +0700

    add starter file for hacker stories website

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr></hr>
+      <List list={stories} />
+    </div>
+  );
+}
+
+const List = (props) => {
+  return (
+    <ul>
+      {props.list.map((item) => {
+        return <Item key={item.objectID} item={item}></Item>;
+      })}
+    </ul>
+  );
+};
+
+const Item = (props) => {
+  return (
+    <li>
+      <span>
+        {" "}
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
+
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange}></input>
+    </div>
+  );
+};
+export default App;
